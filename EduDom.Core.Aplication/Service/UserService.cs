bc75df15bd77eabfa43877eb77ebb98c3f86b787
Carlos Alicea Ben?tez Polo <20186566@itla.edu.do>
2024-08-05 13:08:29 -0400

    Service

---
+++ EduDom.Core.Aplication/Service/UserService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using EduDom.Core.Aplication.Interface.Service;
+using EduDom.Core.Aplication.ViewModels.User;
+using EduDom.Core.Domain.Entities;
+using EduDomAplication.Service;
+using EduDom.Core.Aplication.Interface.Repositories;
+
+
+namespace EduDom.Core.Aplication.Service
+{
+    public class UserService : GenericService <UserSaveViewModels, UserViewModels, User>, IUserService
+    {
+        public UserService(IGenericRepository<User> repository, IMapper mapper)
+            : base(repository, mapper)
+        {
+        }
+    }
+}
